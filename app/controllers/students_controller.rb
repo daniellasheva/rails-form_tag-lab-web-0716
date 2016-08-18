2d0a6a17395e09eeab65de87ee295a0ac887b531
daniellasheva <daniellasheva@gmail.com>
2016-08-18 19:30:04 -0400

    initial commit

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,25 @@
+
+
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
 
   def show
     @student = Student.find(params[:id])
+  end
+
+  def new
+    @student=params
+  end
+
+  def create
+    @student=Student.create(first_name: params[:first_name], last_name: params[:last_name])
+
+    @student.save
+    redirect_to student_path(@student.id)
+    #{}"/student/#{@student.id}"
   end
 
 end
